29e02d820c2e385b499bee815d3b18dd4a74b68c
okks11 <117706958+okks11@users.noreply.github.com>
2022-11-08 12:57:58 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('contextmenu', 
+ event => event.preventDefault()
+);
+
+document.addEventListener("keydown", function (event){
+    if (event.ctrlKey){
+    event.preventDefault();
+    }
+    if(event.keyCode == 123){
+    event.preventDefault();
+    }
+});
